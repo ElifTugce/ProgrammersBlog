0b837b1ab19c2f653a19517853b2c69ad2ac533c
ElifTugce <eliftugcedemir.23@gmail.com>
2022-03-21 15:17:10 +0300

    Add project files.

---
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    //Implementation
+    public class Category:EntityBase, IEntity // bu classımıza data katmanından erişmek istediğimiz için public hale getirdik.
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase // verdiğimiz temel değerlerin diğer sınıflarda değişikliğe uğramasını istiyosak başına abstract koyarız
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual bool IsDeleted { get; set; }
+        public virtual bool IsActive { get; set; }
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; } //String olarak tanımlamamızın sebebi blogumuzda kayıt ol kısmının olmamasıdır. Dolayısıyla kayıt olma olayı olmadığı için string olarak tutarız.
+    }
+
+}
